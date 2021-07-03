257745dd6a2f65d98b452827ca8c28ec7ba7de5e
Juan Jiménez <nefioxdev@gmail.com>
2021-07-03 00:31:17 -0500

    Se muestra el menú dinámicamente

---
+++ controllers/menuController.js
@@ -0,0 +1,37 @@
+const menu = [
+    {
+    id: 1,
+    meal: 'Carpaccio fresco',
+    description: 'Entrada Carpaccio de salmón con cítricos',
+    price: 65.50,
+    image: '/images/Carpaccio-de-salmon.jpg'
+},
+{
+    id: 2,
+    meal: 'Risotto de berenjena',
+    description: 'Risotto de berenjena y queso de cabra',
+    price: 47.00,
+    image: '/images/Risotto-berenjena-queso-cabra.jpg'
+},
+{
+    id: 3,
+    meal: 'Mousse de arroz',
+    description: 'Mousse de arroz con leche y aroma de azahar',
+    price: 27.50,
+    image: '/images/Mousse-de-arroz-con-leche.jpg'
+},
+{
+    id: 4,
+    meal: 'Espárragos blancos',
+    description: 'Espárragos blancos con vinagreta de verduras y jamón ibérico',
+    price: 37.50,
+    image: '/images/esparragos.png'
+}
+]
+
+const controller = {
+    listMeals: (req, res, next) => res.render('index', { menu: menu })
+}
+
+
+module.exports = controller;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const menuController = require('../controllers/menuController')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', menuController.listMeals);
 
 module.exports = router;
